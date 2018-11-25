f44c83f4c048a2406923608dacceab2c329836a0
Eric Dose <astro@ericdose.com>
2018-11-25 00:05:35 -0700

    First viable loop coded, needs testing

--- mpc/scan.py
+++ mpc/scan.py
@@ -5,6 +5,7 @@ import os
 from random import randint
 from time import sleep
 from webbrowser import open_new_tab
+from math import sqrt
 
 import requests
 import pandas as pd
@@ -15,10 +16,9 @@ PAYLOAD_DICT_TEMPLATE = OrderedDict([
     ('ty', 'e'),  # e = 'Return Ephemerides'
     ('TextArea', ''),  # the MP IDs
     ('d', '20181122'),  # first date
-    ('l', '14'),  # number of dates/times
-    # TODO: should be one line for every 30 minutes, not for every 1 hour.
-    ('i', '1'),  # interval between ephemerides
-    ('u', 'h'),  # units of interval; 'h' for hours, 'd' for days
+    ('l', '28'),  # number of dates/times (str of integer)
+    ('i', '30'),  # interval between ephemerides (str of integer)
+    ('u', 'm'),  # units of interval; 'h' for hours, 'd' for days, 'm' for minutes
     ('uto', '0'),  # UTC offset in hours if u=d
     ('long', '-107.55'),  # longitude in deg; make plus sign safe in code below
     ('lat', '+35.45'),  # latitude in deg; make plus sign safe in code below
@@ -44,41 +44,25 @@ GET_HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:64.0) Gecko/
 MIN_TABLE_WORDS = 25
 MIN_ALTITUDE = 30
 MAX_SUN_ALTITUDE = -12
-MAX_V_MAG = 20
+MAX_V_MAG = 20  # ~ optimistic for 3-min exposure, but this is through a clear filter (not V).
+MIN_UNCERTAINTY = 2  # minimum orbit uncertainty to consider following up (in arcseconds).
+MIN_MOON_DIST = 30
 
 
-def get_100_candidates(mp_start=100000, date='20181125'):
+def get_candidates(mp_start=100000, date='20181125', max_mps=10000, max_candidates=100):
     all_dict_list = []
-    html_start = mp_start
-    for i_html in range(100):  # limit number of calls to MPC to get this done:
-        if i_html > 0:
-            sleep(randint(3, 7))  # delay for 3 to 7 seconds (playing nicely with MPC server).
-        this_dict_list = scan_one_html(html_start, date)
-        if len(all_dict_list) >= MAX_MP_PER_HTML:
-            break
-        all_dict_list.extend(this_dict_list)
-        print(str(i_html) + ' ' +
-              str(html_start) + '  + ' +
-              str(len(this_dict_list)) + '   total: ' +
-              str(len(all_dict_list)))
-        html_start += MAX_MP_PER_HTML
-    all_dict_list = all_dict_list[:MAX_MP_PER_HTML]  # truncate if we got a few more than needed.
-
-    # Construct URL and display condensed MPC results in browser:
-    payload_dict = PAYLOAD_DICT_TEMPLATE.copy()
-    mp_list = [item['number'] for item in all_dict_list]
-    payload_dict['TextArea'] = '%0D%0A'.join(mp_list)
-    payload_dict['d'] = date
-    payload_dict['long'] = payload_dict['long'].replace("+", "%2B")  # make safe
-    payload_dict['lat'] = payload_dict['lat'].replace("+", "%2B")    # make safe
-    payload_string = '&'.join([k + '=' + v for (k, v) in payload_dict.items()])
-    url = MPC_URL_STUB + payload_string
-    open_new_tab(url)
+    html_base = mp_start
+    last_mp = mp_start + max_mps - 1
+    while html_base <= last_mp and len(all_dict_list) < max_candidates:
+        n = min(last_mp - html_base + 1, MAX_MP_PER_HTML)
+        lines = get_one_html(start=mp_start, n=n, date=date)
+        html_dict_list = parse_html_lines(lines)
+        all_dict_list.extend(html_dict_list)
+    return all_dict_list
 
 
-def scan_one_html(mp_start=100000, date='20181125'):
+def parse_html_lines(lines):
     mp_dict_list = []
-    lines = get_one_html(start=mp_start, date=date)
     mp_block_limits = chop_html(lines)
     for limits in mp_block_limits:
         mp_dict = extract_mp_data(lines, limits)
@@ -160,7 +144,7 @@ def extract_mp_data(html_lines, mp_block_limits):
         if line.strip().startswith('<p><pre>'):
             if i_line + 1 < mp_block_limits[1]:
                 mp_dict['code'] = html_lines[i_line + 1].strip().split()[0]
-        if line.find('Further observations?') != -1:
+        if 'further observations?' in line.lower():
             mp_dict['status'] = ''
             if line.strip().split('>')[3].strip().lower().startswith('not necessary'):
                 mp_dict['status'] = 'no'
@@ -178,7 +162,8 @@ def extract_mp_data(html_lines, mp_block_limits):
         if table_start_found:
             if '</pre>' in line:
                 break
-        if len(line.split()) >= MIN_TABLE_WORDS:
+        line_split = line.split()
+        if len(line_split) >= MIN_TABLE_WORDS:
             mp_alt = float(line.split()[18])
             if max_mp_alt is None:
                 this_mp_alt_is_max_so_far = True
@@ -186,15 +171,44 @@ def extract_mp_data(html_lines, mp_block_limits):
                 this_mp_alt_is_max_so_far = (mp_alt > max_mp_alt)
             if this_mp_alt_is_max_so_far:
                 max_mp_alt = mp_alt
-                v_mag = float(line.split()[14])
-                sun_alt = float(line.split()[19])
-                # TODO: Also get uncertainty in arcsec (from corrected URL in line) & 'if' on that as well.
-                if mp_alt >= MIN_ALTITUDE and v_mag <= MAX_V_MAG and sun_alt <= MAX_SUN_ALTITUDE:
-                    motion = float(line.split()[15])
-                    mp_dict['v_mag'] = v_mag
-                    mp_dict['motion'] = motion
-                    mp_dict['mp_alt'] = mp_alt
+                v_mag = float(line_split[14])
+                sun_alt = float(line_split[19])
+                moon_dist = float(line_split[21])
+                if mp_alt >= MIN_ALTITUDE and v_mag <= MAX_V_MAG and sun_alt <= MAX_SUN_ALTITUDE and \
+                    moon_dist >= MIN_MOON_DIST:
+                    uncertainty = get_uncertainty(line_split)  # subordinate test as it causes a web call.
+                    if uncertainty >= MIN_UNCERTAINTY:
+                        motion = float(line_split[15])
+                        mp_dict['v_mag'] = v_mag
+                        mp_dict['motion'] = motion
+                        mp_dict['mp_alt'] = mp_alt
+                        mp_dict['uncert'] = uncertainty
+                        mp_dict['utc'] = ' '.join(line_split[0:4])
+                        mp_dict['ra'] = ':'.join(line_split[4:7])
+                        mp_dict['dec'] = ':'.join(line_split[7:10])
+                        print(mp_dict)
     return mp_dict
+
+
+def get_uncertainty(line_split):
+    url = line_split[-1].split('href=\"')[-1].split('&OC')[0]
+    sleep(0.5 * randint(1, 3))  # delay between calls (playing nicely with MPC server).
+    lines = requests.get(url, headers=GET_HEADER).text.splitlines()
+    pre_line_found = False
+    uncertainties_2 = []
+    for line in lines:
+        if line.strip().lower().startswith('</pre>'):
+            break
+        if line.strip().lower().startswith('<pre>'):
+            pre_line_found = True
+            continue
+        if pre_line_found:
+            words = line.split()
+            if len(words) >= 3 and 'variant orbit' in line.lower():
+                this_uncertainty_2 = float(words[0])**2 + float(words[1])**2
+                uncertainties_2.append(this_uncertainty_2)
+    uncertainties_2.sort(reverse=True)  # in-place.
+    return sqrt(uncertainties_2[0] + uncertainties_2[1])
 
 
 def t():
@@ -218,7 +232,44 @@ def web(start=200000, n=50, date='20181201'):
         print(mp_dict)
 
 
-
-
 if __name__ == "__main__":
     get_one_html()
+
+
+# *******************************************************************************************
+    # for i_html in range(100):  # limit number of calls to MPC to get this done:
+    #     if i_html > 0:
+    #         sleep(randint(3, 7))  # delay for 3 to 7 seconds (playing nicely with MPC server).
+    #     this_dict_list = scan_one_html(html_start, date)
+    #     if len(all_dict_list) >= MAX_MP_PER_HTML:
+    #         break
+    #     all_dict_list.extend(this_dict_list)
+    #     print(str(i_html) + ' ' +
+    #           str(html_start) + '  + ' +
+    #           str(len(this_dict_list)) + '   total: ' +
+    #           str(len(all_dict_list)))
+    #     html_start += MAX_MP_PER_HTML
+    # all_dict_list = all_dict_list[:MAX_MP_PER_HTML]  # truncate if we got a few more than needed.
+    #
+    # # Construct URL and display condensed MPC results in browser:
+    # payload_dict = PAYLOAD_DICT_TEMPLATE.copy()
+    # mp_list = [item['number'] for item in all_dict_list]
+    # payload_dict['TextArea'] = '%0D%0A'.join(mp_list)
+    # payload_dict['d'] = date
+    # payload_dict['long'] = payload_dict['long'].replace("+", "%2B")  # make safe
+    # payload_dict['lat'] = payload_dict['lat'].replace("+", "%2B")    # make safe
+    # payload_string = '&'.join([k + '=' + v for (k, v) in payload_dict.items()])
+    # url = MPC_URL_STUB + payload_string
+    # open_new_tab(url)
+#
+#
+# def scan_one_html(mp_start=100000, date='20181125'):
+#     mp_dict_list = []
+#     lines = get_one_html(start=mp_start, date=date)
+#     mp_block_limits = chop_html(lines)
+#     for limits in mp_block_limits:
+#         mp_dict = extract_mp_data(lines, limits)
+#         if mp_dict.get('v_mag', None) is not None:
+#             mp_dict_list.append(mp_dict)
+#             print(mp_dict['number'])
+#     return mp_dict_list
