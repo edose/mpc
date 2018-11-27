7d02feb377ad245147b60a1a6009a5651890e3ae
Eric Dose <astro@ericdose.com>
2018-11-26 19:04:14 -0700

    Working, delivers a DataFrame

--- mpc/scan.py
+++ mpc/scan.py
@@ -6,6 +6,7 @@ from random import randint
 from time import sleep
 from webbrowser import open_new_tab
 from math import sqrt
+from datetime import datetime, timezone, timedelta
 
 import requests
 import pandas as pd
@@ -44,21 +45,31 @@ GET_HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:64.0) Gecko/
 MIN_TABLE_WORDS = 25
 MIN_ALTITUDE = 30
 MAX_SUN_ALTITUDE = -12
-MAX_V_MAG = 20  # ~ optimistic for 3-min exposure, but this is through a clear filter (not V).
+MAX_V_MAG = 19.0  # slightly optimistic for 3-min exposure, but this would be through Clear filter (not V).
 MIN_UNCERTAINTY = 2  # minimum orbit uncertainty to consider following up (in arcseconds).
 MIN_MOON_DIST = 30
 
 
-def get_candidates(mp_start=100000, date='20181125', max_mps=10000, max_candidates=100):
+def go(mp_start=100000, date_utc=None, max_mps=10000, max_candidates=100):
+    if date_utc is None:
+        target_date = datetime.now(timezone.utc) + timedelta(days=1)
+        date_string = '{0:04d}{1:02d}{2:02d}'.format(target_date.year, target_date.month, target_date.day)
+    else:
+        date_string = date_utc
     all_dict_list = []
     html_base = mp_start
     last_mp = mp_start + max_mps - 1
     while html_base <= last_mp and len(all_dict_list) < max_candidates:
-        n = min(last_mp - html_base + 1, MAX_MP_PER_HTML)
-        lines = get_one_html(start=mp_start, n=n, date=date)
+        n_mps_this_html = min(last_mp - html_base + 1, MAX_MP_PER_HTML)
+        print(str(html_base), str(html_base + n_mps_this_html - 1), str(n_mps_this_html),
+              end='', flush=True)
+        lines = get_one_html(start=html_base, n=n_mps_this_html, date=date_string)
         html_dict_list = parse_html_lines(lines)
         all_dict_list.extend(html_dict_list)
-    return all_dict_list
+        html_base += MAX_MP_PER_HTML
+        print(' --> ' + str(len(all_dict_list)))
+    print('Done.')
+    return pd.DataFrame(all_dict_list)
 
 
 def parse_html_lines(lines):
@@ -68,7 +79,8 @@ def parse_html_lines(lines):
         mp_dict = extract_mp_data(lines, limits)
         if mp_dict.get('v_mag', None) is not None:
             mp_dict_list.append(mp_dict)
-            print(mp_dict['number'])
+            # print(mp_dict['number'])
+            # print(mp_dict)
     return mp_dict_list
 
 
@@ -92,7 +104,7 @@ def get_one_html(start=200000, n=MAX_MP_PER_HTML, date='20181122'):
     url = MPC_URL_STUB + payload_string
 
     # Make GET call, parse return text.
-    print(url)
+    # print(url)
     r = requests.get(url, headers=GET_HEADER)
     return r.text.splitlines()
 
@@ -150,11 +162,13 @@ def extract_mp_data(html_lines, mp_block_limits):
                 mp_dict['status'] = 'no'
             if line.strip().split('>')[3].strip().lower().startswith('useful'):
                 mp_dict['status'] = 'USEFUL'
+    mp_dict_short = mp_dict.copy()
 
     # Handle items in ephemeris table. Use line with highest altitude:
     # Find line limits of ephemeris table (slight superset from <pre> and </pre> HTML tags:
     table_start_found = False
     max_mp_alt = None
+    uncertainty_raw_url = ''  # to keep IDE happy
     for i_line in range(mp_block_limits[0], mp_block_limits[1]):
         line = html_lines[i_line]
         if '<pre>' in line:
@@ -165,34 +179,37 @@ def extract_mp_data(html_lines, mp_block_limits):
         line_split = line.split()
         if len(line_split) >= MIN_TABLE_WORDS:
             mp_alt = float(line.split()[18])
-            if max_mp_alt is None:
-                this_mp_alt_is_max_so_far = True
-            else:
-                this_mp_alt_is_max_so_far = (mp_alt > max_mp_alt)
-            if this_mp_alt_is_max_so_far:
-                max_mp_alt = mp_alt
-                v_mag = float(line_split[14])
-                sun_alt = float(line_split[19])
-                moon_dist = float(line_split[21])
-                if mp_alt >= MIN_ALTITUDE and v_mag <= MAX_V_MAG and sun_alt <= MAX_SUN_ALTITUDE and \
-                    moon_dist >= MIN_MOON_DIST:
-                    uncertainty = get_uncertainty(line_split)  # subordinate test as it causes a web call.
-                    if uncertainty >= MIN_UNCERTAINTY:
-                        motion = float(line_split[15])
-                        mp_dict['v_mag'] = v_mag
-                        mp_dict['motion'] = motion
-                        mp_dict['mp_alt'] = mp_alt
-                        mp_dict['uncert'] = uncertainty
-                        mp_dict['utc'] = ' '.join(line_split[0:4])
-                        mp_dict['ra'] = ':'.join(line_split[4:7])
-                        mp_dict['dec'] = ':'.join(line_split[7:10])
-                        print(mp_dict)
+            v_mag = float(line_split[14])
+            sun_alt = float(line_split[19])
+            moon_dist = float(line_split[21])
+            motion = float(line_split[15])
+            if mp_alt >= MIN_ALTITUDE and v_mag <= MAX_V_MAG and sun_alt <= MAX_SUN_ALTITUDE and \
+                moon_dist >= MIN_MOON_DIST:
+                if max_mp_alt is None:
+                    this_mp_alt_is_max_so_far = True
+                else:
+                    this_mp_alt_is_max_so_far = (mp_alt > max_mp_alt)
+                if this_mp_alt_is_max_so_far:
+                    max_mp_alt = mp_alt
+                    mp_dict['v_mag'] = v_mag
+                    mp_dict['motion'] = motion
+                    mp_dict['mp_alt'] = mp_alt
+                    mp_dict['utc'] = ' '.join(line_split[0:4])
+                    mp_dict['ra'] = ':'.join(line_split[4:7])
+                    mp_dict['dec'] = ':'.join(line_split[7:10])
+                    uncertainty_raw_url = line_split[-1]
+    if mp_dict.get('v_mag', None) is not None:
+        uncertainty = get_uncertainty(uncertainty_raw_url)
+        if uncertainty >= MIN_UNCERTAINTY:
+            mp_dict['uncert'] = uncertainty
+        else:
+            mp_dict = mp_dict_short
     return mp_dict
 
 
-def get_uncertainty(line_split):
-    url = line_split[-1].split('href=\"')[-1].split('&OC')[0]
-    sleep(0.5 * randint(1, 3))  # delay between calls (playing nicely with MPC server).
+def get_uncertainty(raw_url):
+    url = raw_url.split('href=\"')[-1].split('&OC')[0]
+    # print(url)
     lines = requests.get(url, headers=GET_HEADER).text.splitlines()
     pre_line_found = False
     uncertainties_2 = []
